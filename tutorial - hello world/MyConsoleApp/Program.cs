fb4d948766cbc1991a5229cbe318776820001e1e
ayusograce <adriayusof@gmail.com>
2026-05-12 23:55:22 -0500

    week1

---
+++ tutorial - hello world/MyConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿var time = DateTime.Now;
+var christmas = new DateTime(time.Year, 12, 25);
+var days = (christmas - time).Days;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {time}.");
+Console.WriteLine($"There are {days} days until the next Christmas.");
